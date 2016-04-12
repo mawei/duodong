ec9f6b829eba4e7af3a37bed2eb365de9c7af0f9
pony <mawei78951@126.com>
2016-04-12 13:58:54 +0800

    database config

--- application/config/database.php
+++ application/config/database.php
@@ -51,7 +51,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'shanshan78951';
 $db['default']['database'] = 'duodong';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
